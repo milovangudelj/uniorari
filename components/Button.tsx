550ce9cfa0d69dc215c0f26a719b9f2cc1716a40
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-11-30 03:01:31 +0100

    Fixed invisible button

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,7 +3,7 @@ export const Button = ({ children, variant = "primary", ...props }) => {
 		<button
 			{...props}
 			className={
-				props.className +
+				props?.className +
 				(!props.external
 					? ` text-body-l text-on-primary-he bg-${variant}-500 hover:bg-${variant}-400 focus:bg-${variant}-600 transition py-2 px-4 rounded-lg`
 					: "")
