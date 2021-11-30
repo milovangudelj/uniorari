8f6057e7a4834508bc78098a34b6b2da7992104b
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-11-30 03:20:49 +0100

    Still fighting against the button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from "react";
+
 export const Button = ({
 	children,
 	variant = "primary",
 	size = "regular",
 	...props
 }) => {
+	const [bgStyles, setBgStyles] = useState({
+		primary: "bg-primary-500 hover:bg-primary-400 focus:bg-primary-600",
+		accent: "bg-accent-500 hover:bg-accent-400 focus:bg-accent-600",
+		success: "bg-success-500 hover:bg-success-400 focus:bg-success-600",
+		error: "bg-error-500 hover:bg-error-400 focus:bg-error-600",
+	});
+
 	return (
 		<button
 			{...props}
 			className={
 				(props.className ? props.className : "") +
 				(!props.external
-					? ` text-body-l text-on-primary-he bg-${variant}-500 hover:bg-${variant}-400 focus:bg-${variant}-600 transition py-2 px-4 rounded-lg`
+					? ` text-body-l text-on-primary-he ${bgStyles[variant]} transition py-2 px-4 rounded-lg`
 					: "")
 			}
 		>
