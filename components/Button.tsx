cbfab8bee7100f6a7e3b7bfe97dc19df41aba446
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-11-30 03:04:10 +0100

    Maybe this time I really fixed it

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,14 @@
-export const Button = ({ children, variant = "primary", ...props }) => {
+export const Button = ({
+	children,
+	variant = "primary",
+	size = "regular",
+	...props
+}) => {
 	return (
 		<button
 			{...props}
 			className={
-				props?.className +
+				(props.className ? props.className : "") +
 				(!props.external
 					? ` text-body-l text-on-primary-he bg-${variant}-500 hover:bg-${variant}-400 focus:bg-${variant}-600 transition py-2 px-4 rounded-lg`
 					: "")
