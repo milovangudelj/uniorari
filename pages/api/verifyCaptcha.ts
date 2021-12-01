3bf232dbb77bcd08853bfed1b36ec77552013f87
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-12-01 13:09:48 +0100

    I've added the env variable to vercel

--- pages/api/verifyCaptcha.ts
+++ pages/api/verifyCaptcha.ts
@@ -2,17 +2,15 @@ export default async function handler(req, res) {
 	const { token } = req.query;
 
 	const response = await fetch(
-		`https://www.google.com/recaptcha/api/siteverify`,
-		{
-			method: "POST",
-			body: `secret=${
-				process.env.NODE_ENV === "development"
-					? process.env.TEST_RECAPTCHA_SECRET
-					: process.env.RECAPTCHA_SECRET
-			}&response=${token}`,
-		}
+		`https://www.google.com/recaptcha/api/siteverify?secret=${
+			process.env.NODE_ENV === "development"
+				? process.env.TEST_RECAPTCHA_SECRET
+				: process.env.RECAPTCHA_SECRET
+		}&response=${token}`
 	);
+
 	const data = await response.json();
+
 	console.log(
 		process.env.NODE_ENV === "development"
 			? process.env.TEST_RECAPTCHA_SECRET
