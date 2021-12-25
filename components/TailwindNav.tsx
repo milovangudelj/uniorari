f56076195ad6789299cc0190e3518cdaff73f0fd
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-12-25 02:20:16 +0100

    Removed bell

--- components/TailwindNav.tsx
+++ components/TailwindNav.tsx
@@ -88,7 +88,7 @@ export const TailwindNav = () => {
 							<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 								<button
 									type="button"
-									className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+									className="hidden md:inline bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
 								>
 									<span className="sr-only">View notifications</span>
 									<BellIcon className="h-6 w-6" aria-hidden="true" />
@@ -98,8 +98,8 @@ export const TailwindNav = () => {
 								<Menu as="div" className="ml-3 relative">
 									<div>
 										<Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-											<span className="sr-only">Open user menu</span>
-											{/* <img
+											{/* <span className="sr-only">Open user menu</span>
+												<img
 												className="h-8 w-8 rounded-full"
 												src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 												alt=""
@@ -123,7 +123,7 @@ export const TailwindNav = () => {
 											) : (
 												<div>
 													<Link href="/accedi">
-														<a className="mr-4 text-on-primary-me hover:text-on-primary-he transition">
+														<a className="hidden md:inline mr-4 text-on-primary-me hover:text-on-primary-he transition">
 															Accedi
 														</a>
 													</Link>
