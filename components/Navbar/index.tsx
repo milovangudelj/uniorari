6a2bb09447042348eb7c31ce7fb282a8b66c55a5
Milovn Gudelj <milovan.gudelj@gmail.com>
2021-12-28 03:01:29 +0100

    Took out nav in separate components

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -5,6 +5,9 @@ import { useRouter } from "next/router";
 import { useAuth } from "../../lib/auth";
 import { Button } from "..";
 import ProfileMenu from "./ProfileMenu";
+import { useDevice } from "../../lib/device";
+import DesktopNav from "./DesktopNav";
+import MobileNav from "./MobileNav";
 
 const pages = [
 	{
@@ -25,46 +28,28 @@ export const Navbar = () => {
 	const router = useRouter();
 
 	const { user, loading, signOut } = useAuth();
+	const { width } = useDevice();
 
 	return (
 		<>
 			<div className="w-full bg-white border-b border-grey-100 flex justify-center sticky top-0 z-20">
-				<header className="max-w-7xl w-full flex items-center px-2 py-3">
-					<Link href="/">
-						<img
-							src="/logo.svg"
-							alt="Logo UniOrari"
-							title="UniOrari"
-							className="mr-8 cursor-pointer"
-						/>
-					</Link>
-					<nav className="mr-auto">
-						<ul className="flex text-body-l">
-							{pages.map((page, idx) => (
-								<li
-									key={idx}
-									className={`pr-4 ${
-										router.pathname === page.link
-											? "text-accent-500 hover:text-accent-700"
-											: "text-on-surface-me hover:text-on-surface-he"
-									} transition`}
-								>
-									<Link href={page.link}>
-										<a>{page.name}</a>
-									</Link>
-								</li>
-							))}
-						</ul>
-					</nav>
+				<header className="max-w-7xl w-full flex items-center justify-between px-4 py-3">
+					{width <= 640 ? (
+						<MobileNav pages={pages} current={router.pathname} />
+					) : (
+						<DesktopNav pages={pages} current={router.pathname} />
+					)}
 					{user ? (
 						<ProfileMenu user={user} signOut={signOut} />
 					) : (
 						<div>
-							<Link href="/accedi">
-								<a className="mr-4 text-on-surface-me hover:text-on-surface-he transition">
-									Accedi
-								</a>
-							</Link>
+							{width > 640 && (
+								<Link href="/accedi">
+									<a className="mr-4 text-on-surface-me hover:text-on-surface-he transition">
+										Accedi
+									</a>
+								</Link>
+							)}
 							<Button variant="primary">
 								<Link href="/iscriviti">Iscriviti</Link>
 							</Button>
