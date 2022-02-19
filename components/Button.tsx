0107c68be25c6d2b5d471500629b107896b9302e
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-02-19 15:56:28 +0100

    Now links work again

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,8 +11,8 @@ import { ConditionalWrapper } from ".";
 interface ButtonProps<T extends ElementType> {
 	as?: T;
 	children?: ReactNode;
-	className?: string;
-	disabled?: boolean;
+	href?: string;
+	passHref?: boolean;
 	external?: boolean;
 	pill?: boolean;
 	loading?: boolean;
@@ -44,8 +44,8 @@ const classes = {
 
 export const Button = <T extends ElementType = "button">({
 	as,
-	className = "",
-	disabled = false,
+	href,
+	passHref,
 	external = false,
 	variant = "contained",
 	color = "primary",
@@ -55,55 +55,68 @@ export const Button = <T extends ElementType = "button">({
 	...props
 }: ButtonProps<T> &
 	Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>) => {
+	const Component = as || "button";
 	const [style, setStyle] = useState<string>(
 		!external
 			? cls(
 					`${classes.base} ${classes.size[size]} ${
 						classes.variant(color)[variant]
 					} ${classes.pill(pill)} ${classes.disabled(
-						disabled
-					)} ${classes.loading(loading)} ${className}`
+						props.disabled
+					)} ${classes.loading(loading)} ${props.className}`
 			  )
-			: className
+			: props.className
 	);
-	const Component = as || "button";
+
+	const content = (
+		<>
+			{loading && (
+				<svg
+					className={`motion-safe:animate-spin absolute ${
+						size === "small" ? "h-4 w-4" : "h-5 w-5"
+					}`}
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+				>
+					<circle
+						className="opacity-25"
+						cx="12"
+						cy="12"
+						r="10"
+						stroke="currentColor"
+						strokeWidth="4"
+					></circle>
+					<path
+						className="opacity-75"
+						fill="currentColor"
+						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+					></path>
+				</svg>
+			)}
+			<span className={loading ? "invisible" : ""}>{props.children}</span>
+		</>
+	);
 
 	return (
-		<Component {...props} disabled={disabled || loading} className={style}>
-			<ConditionalWrapper
-				condition={props.passHref}
-				wrapper={(children) => (
-					<a {...props} disabled={disabled || loading} className={style}>
-						{children}
-					</a>
-				)}
-			>
-				{loading && (
-					<svg
-						className={`motion-safe:animate-spin absolute ${
-							size === "small" ? "h-4 w-4" : "h-5 w-5"
-						}`}
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-					>
-						<circle
-							className="opacity-25"
-							cx="12"
-							cy="12"
-							r="10"
-							stroke="currentColor"
-							strokeWidth="4"
-						></circle>
-						<path
-							className="opacity-75"
-							fill="currentColor"
-							d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-						></path>
-					</svg>
-				)}
-				<span className={loading ? "invisible" : ""}>{props.children}</span>
-			</ConditionalWrapper>
+		<Component
+			{...props}
+			href={href}
+			passHref={passHref}
+			disabled={props.disabled || loading}
+			className={style}
+		>
+			{passHref ? (
+				<a
+					{...props}
+					disabled={props.disabled || loading}
+					className={style}
+				>
+					{content}
+				</a>
+			) : (
+				content
+			)}
 		</Component>
 	);
 };
