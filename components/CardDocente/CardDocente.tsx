2b794f503291454163d427644d6d250af51d6a5d
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-03-05 03:07:38 +0100

    Changed card layout

--- components/CardDocente/CardDocente.tsx
+++ components/CardDocente/CardDocente.tsx
@@ -12,33 +12,45 @@ export const CardDocente = ({ data, ...props }) => {
 	}, [data]);
 
 	return (
-		<div className="bg-grey-50 shadow rounded-lg overflow-hidden flex">
-			<span className="inline-block relative aspect-[3/4] h-full bg-gray-300">
-				<Image
-					src={
-						docente.immagine || "/assets/images/profile-placeholder.webp"
-					}
-					alt={`${docente.nome} ${docente.cognome}`}
-					layout="fill"
-					objectFit="cover"
-				/>
-			</span>
-			<div className="p-8">
-				<div className="mb-8">
-					<h2 className="text-2xl text-on-surface-he mr-4">{`${docente.nome} ${docente.cognome}`}</h2>
-					<span className="text-sm text-accent-600">{docente.email}</span>
-				</div>
+		<div className="bg-grey-50 shadow rounded-lg overflow-hidden">
+			<div className="flex items-center p-4">
+				<span className="inline-block relative aspect-square h-16 rounded-full overflow-hidden mr-4 bg-gray-300">
+					<Image
+						src={
+							docente.immagine ||
+							"/assets/images/profile-placeholder.webp"
+						}
+						alt={`${docente.nome} ${docente.cognome}`}
+						layout="fill"
+						objectFit="cover"
+					/>
+				</span>
 				<div>
-					<span className="text-sm font-medium">Corsi</span>
-					<div className="mt-2">
-						{docente.corsi.map((corso) => {
+					<h2 className="text-2xl text-gray-900 mr-4">{`${docente.nome} ${docente.cognome}`}</h2>
+					<a
+						href={`mailto:${docente.email}`}
+						target="_blank"
+						rel="noreferrer"
+						className="text-sm text-accent-600"
+					>
+						{docente.email}
+					</a>
+				</div>
+			</div>
+			<div className="p-4 text-gray-900 bg-gray-100">
+				<span className="text-sm font-medium">Corsi</span>
+				<div className="mt-2 flex gap-1">
+					{docente.corsi.length !== 0 ? (
+						docente.corsi.map((corso) => {
 							return (
-								<Chip key={corso.id} size="normal">
-									{corso.insegnamento.nome}
+								<Chip key={corso.id} size="small">
+									{corso.nome}
 								</Chip>
 							);
-						})}
-					</div>
+						})
+					) : (
+						<span className="text-gray-500">Nessun corso seguito</span>
+					)}
 				</div>
 			</div>
 		</div>
