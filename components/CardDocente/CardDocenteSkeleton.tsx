5f41705f6d5d8cfc1de2350162bacd3d370beb34
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-03-10 17:27:23 +0100

    Swapped image for loading gradient

--- components/CardDocente/CardDocenteSkeleton.tsx
+++ components/CardDocente/CardDocenteSkeleton.tsx
@@ -5,14 +5,7 @@ export const CardDocenteSkeleton = () => {
 	return (
 		<div className="bg-grey-50 dark:bg-grey-700 shadow rounded-lg overflow-hidden">
 			<div className="flex items-center p-4 gap-4">
-				<span className="block relative w-16 aspect-square rounded-full overflow-hidden bg-grey-300 dark:bg-grey-700">
-					<Image
-						src={"/assets/images/profile-placeholder.webp"}
-						alt={`Placeholder immagine profilo`}
-						layout="fill"
-						objectFit="cover"
-					/>
-				</span>
+				<span className="block relative w-16 aspect-square rounded-full overflow-hidden bg-skeleton bg-dark-skeleton-lighter animate-skeleton"></span>
 				<div className="w-min flex flex-col md:w-max">
 					<Placeholder size="2xl" shade="emphasized" className="w-40" />
 					<Placeholder size="sm" shade="emphasized" />
