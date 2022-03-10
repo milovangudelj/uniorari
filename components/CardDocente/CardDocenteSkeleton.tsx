6eacb5c2daadf7a69cc965e419b7c078848bc64c
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-03-10 13:30:00 +0100

    Added skeleton for teacher card

---
+++ components/CardDocente/CardDocenteSkeleton.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Placeholder } from "..";
+
+export const CardDocenteSkeleton = () => {
+	return (
+		<div className="bg-grey-50 dark:bg-grey-700 shadow rounded-lg overflow-hidden">
+			<div className="flex items-center p-4 gap-4">
+				<span className="block relative w-16 aspect-square rounded-full overflow-hidden bg-grey-300 dark:bg-grey-700">
+					<Image
+						src={"/assets/images/profile-placeholder.webp"}
+						alt={`Placeholder immagine profilo`}
+						layout="fill"
+						objectFit="cover"
+					/>
+				</span>
+				<div className="w-min flex flex-col md:w-max">
+					<Placeholder size="2xl" shade="lighter" className="w-40" />
+					<Placeholder size="sm" shade="lighter" />
+				</div>
+			</div>
+			<div className="p-4 h-full bg-grey-100 dark:bg-grey-800">
+				<span className="text-sm font-medium">Corsi</span>
+				<div className="mt-2 flex gap-1">
+					<Placeholder />
+				</div>
+			</div>
+		</div>
+	);
+};
--- components/CardDocente/index.ts
+++ components/CardDocente/index.ts
@@ -1,1 +1,2 @@
 export * from "./CardDocente";
+export * from "./CardDocenteSkeleton";
