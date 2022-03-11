acca1af6527302d6c42eaf86ad5fd32f5caee85a
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-03-11 21:46:07 +0100

    Dark theme babyyy

--- pages/verifica-email.tsx
+++ pages/verifica-email.tsx
@@ -2,23 +2,28 @@ import Head from "next/head";
 import Link from "next/link";
 import { Button } from "../components";
 
-export default function verifyRequest({ baseUrl }) {
+const VerificaEmail = ({ baseUrl }) => {
 	return (
-		<div className="w-screen h-screen flex items-center justify-center">
+		<>
 			<Head>
 				<title>Verifica | UniOrari</title>
 				<meta name="description" content="Orari delle lezioni" />
 			</Head>
-			<main className="text-body-l text-on-surface-he">
-				<h1 className="text-display-s">Controlla la tua mail</h1>
-				<p className="text-on-surface-me mt-2 mb-16">
-					Per accedere clicca il pulsante che ti abbiamo inviato per mail.
-				</p>
+			<div className="w-screen bg-grey-50 dark:bg-grey-900 h-screen flex items-center justify-center">
+				<main className="text-body-l text-on-surface-he dark:text-on-primary-he">
+					<h1 className="text-display-s">Controlla la tua mail</h1>
+					<p className="text-on-surface-me dark:text-on-primary-me mt-2 mb-16">
+						Per accedere clicca il pulsante che ti abbiamo inviato per
+						mail.
+					</p>
 
-				<Button as={Link} passHref href="/">
-					Torna alla Home
-				</Button>
-			</main>
-		</div>
+					<Button as={Link} passHref href="/">
+						Torna alla Home
+					</Button>
+				</main>
+			</div>
+		</>
 	);
-}
+};
+
+export default VerificaEmail;
