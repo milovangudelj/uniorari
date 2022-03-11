ed669f5405b69685210ac4116f6eb3ec7f0ad5a4
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-03-11 15:50:40 +0100

    Added merge policy for courses field of `Profilo`

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -3,7 +3,19 @@ import { PATH } from "./constants";
 
 const apolloClient = new ApolloClient({
 	uri: PATH + "/api/graphql",
-	cache: new InMemoryCache(),
+	cache: new InMemoryCache({
+		typePolicies: {
+			Profilo: {
+				fields: {
+					corsi: {
+						merge(existing = [], incoming: any) {
+							return { ...existing, ...incoming };
+						},
+					},
+				},
+			},
+		},
+	}),
 });
 
 export default apolloClient;
