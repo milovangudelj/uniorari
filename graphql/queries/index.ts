ea1e371061179895c34d7e8e090bcd0326ea2d47
Milovn Gudelj <milovan.gudelj@gmail.com>
2022-03-12 01:10:55 +0100

    Removed superfluous query

--- graphql/queries/index.ts
+++ graphql/queries/index.ts
@@ -1,6 +1,5 @@
 export * from "./queryInsegnamenti";
 export * from "./queryDocenti";
 export * from "./queryCorsiSalvati";
-export * from "./queryIdsCorsiSalvati";
 export * from "./addCourseToProfile";
 export * from "./removeCourseFromProfile";
