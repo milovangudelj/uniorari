2fb0b0cafe991eb078f1f49babf69fbd85773d29
milovangudelj <milovan.gudelj@gmail.com>
2023-02-10 20:57:54 +0100

    New server logic for retrieving user

--- pages/api/corsi-salvati.ts
+++ pages/api/corsi-salvati.ts
@@ -3,6 +3,16 @@ import apolloClient from "../../lib/apollo";
 import { queryCorsiSalvati } from "../../graphql/queries";
 
 export default async function handler(req, res) {
+	const refreshToken = req.cookies["uo-refresh-token"];
+	const accessToken = req.cookies["uo-access-token"];
+
+	if (refreshToken && accessToken) {
+		await supabase.auth.setSession({
+			refresh_token: refreshToken,
+			access_token: accessToken,
+		});
+	}
+
 	const {
 		data: { user },
 	} = await supabase.auth.getUser();
